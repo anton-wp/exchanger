5aa37560af9205f0dca98e1cd7b54581c4de0b05
Anton <antony.gorbenko@gmail.com>
2021-01-12 15:17:38 +0200

    static

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,22 @@ import InputBlock from '../components/inputBlock'
 import InteractiveBlock from '../components/interactiveBlock'
 import { BASE_URL } from '../components/const'
 import { useState } from 'react';
+import { BankOutlined } from '@ant-design/icons';
 
 export default function Home({ currency }) {
   const [base, setBase] = useState({ ...currency[0] })
 
 
   return (
-    <div className="container">
-      <InputBlock currency={currency} base={base} setBase={(e) => setBase(e)} />
-      <InteractiveBlock base={base} />
-    </div>
+    <>
+      <div className="menu">
+        <BankOutlined className="logo" />
+      </div>
+      <div className="container">
+        <InputBlock currency={currency} base={base} setBase={(e) => setBase(e)} />
+        <InteractiveBlock base={base} />
+      </div>
+    </>
   )
 }
 Home.getInitialProps = async () => {
